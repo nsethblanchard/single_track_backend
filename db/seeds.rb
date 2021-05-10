8fc3ec456e10af9e9a86f48238273fef4fd00284
Seth Blanchard <nsethblanchard@gmail.com>
2021-05-10 09:15:56 -0400

    seed data for store

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+store1 = Store.create(name: "Black Dog Bikes", phone: "540-887-8700", city: "Staunton")
+store2 = Store.create(name: "Shenandoah Bicycle Company", phone: "540-437-9000", city: "Harrisonburg")
+store3 = Store.create(name: "Rocktown Bicycyles", phone: "540-689-0070", city: "Harrisonburg")
