6cac850efeb93ed903b4b523828460bf59a2229a
Seth Blanchard <nsethblanchard@gmail.com>
2021-05-11 14:07:07 -0400

    store strong params and further action manipulation

--- app/controllers/api/stores_controller.rb
+++ app/controllers/api/stores_controller.rb
@@ -7,6 +7,12 @@ class Api::StoresController < ApplicationController
 
     def create
         store = Store.new(store_params)
+        if store.save
+            render json: store, status: :accepted
+        else
+            render json: {errors: store.errors.full_messages}, status: :unprocessable_entity
+            # server understands context and syntax was correct but was unable to process (error 422 -unprocessable_entity)
+        end
 
     end
 
