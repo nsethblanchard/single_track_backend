72b240162235de9385199f9c0069f67ca5a3e1e2
Seth Blanchard <nsethblanchard@gmail.com>
2021-05-11 13:56:14 -0400

    created some store controller actions

--- app/controllers/api/stores_controller.rb
+++ app/controllers/api/stores_controller.rb
@@ -1,4 +1,18 @@
 class Api::StoresController < ApplicationController
 
-    
+    def index
+        stores = Store.all
+        render json: stores
+    end
+
+    def create
+        store = Store.new(store_params)
+
+    end
+
+    private
+
+    def store_params
+        params.require(:store).permit(:name, :phone, :city)
+    end
 end
