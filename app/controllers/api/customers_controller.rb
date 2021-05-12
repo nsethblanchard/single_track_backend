4ed97be316c24c3f2d6cc076f899b268990702f9
Seth Blanchard <nsethblanchard@gmail.com>
2021-05-12 09:22:51 -0400

    added some actions in customer controller and serializer

---
+++ app/controllers/api/customers_controller.rb
@@ -0,0 +1,25 @@
+class Api::CustomersController < ApplicationController
+    def index
+        customers = Customer.all
+        render json: CustomerSerializer.new(customers)
+    end
+
+    def create
+        customer = Customer.new(customer_params)
+        if customer.save
+            render json: customer, status: :accepted
+        else
+            render json: {errors: customer.errors.full_messages}, status: :unprocessable_entity
+            # server understands context and syntax was correct but was unable to process (error 422 -unprocessable_entity)
+        end
+
+    end
+
+    private
+
+    def customer_params
+        params.require(:customer).permit(:store_id, :name, :email, :phone, :stravaURL, :bikeStyle)
+    end
+end
+
+end
--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,4 +1,4 @@
 class CustomerSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :store_id, :name, :email, :phone, :stravaURL, :bikeStyle
+  attributes :store_id, :name, :email, :phone, :stravaURL, :bikeStyle, :store
 end
