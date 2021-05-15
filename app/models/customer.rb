a61203e5e6392305cd16f0c30234247b68eb3bf3
Seth Blanchard <nsethblanchard@gmail.com>
2021-05-15 09:52:41 -0400

    added validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
   belongs_to :store
   has_many :items, dependent: :destroy
+
+  validates :name, :email, :phone, :stravaURL, :bikeStyle, presence: true
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -2,4 +2,5 @@ class Store < ApplicationRecord
     has_many :customers, dependent: :destroy
     has_many :items, through: :customers, dependent: :destroy
     
+    validates :name, :phone, :city, presence: true
 end
